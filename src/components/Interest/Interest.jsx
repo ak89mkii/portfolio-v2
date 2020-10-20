f16b2f181da23343ae4345f7e78a1b21176151c9
A.I. Kasem <ak89mkii@gmail.com>
2020-10-20 15:39:21 -0400

    Added components

--- src/components/Interest/Interest.jsx
+++ src/components/Interest/Interest.jsx
@@ -7,8 +7,8 @@ const Interest = () => {
         <div className='CircleSelector'>
             <Divider horizontal>
               <Header as='h4'>
-                <Icon name='left quote' />
-                Mission Statement
+                <Icon name='info' />
+                Interests
               </Header>
             </Divider>
             <Message floating content='As a software engineer, I endeavor to work on projects that require a synergy of logic, analytical methodologies, and creativity. I have experience in entrepreneurial development, product development / design, as well as tabletop game design. I have honed my leadership skills through the military and research acumen while earning a bachelors of science degree in applied psychology.
