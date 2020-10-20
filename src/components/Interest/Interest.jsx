165f56bc7630ac9f1a8a711db9fa94197d8bab00
A.I. Kasem <ak89mkii@gmail.com>
2020-10-20 14:25:30 -0400

    Added components

--- src/components/Interest/Interest.jsx
+++ src/components/Interest/Interest.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
+import { Header, Icon, Image, Menu, Message, Divider } from 'semantic-ui-react'
 
 
-const Interest = (props) => {
+const Interest = () => {
     return (
-        
         <div className='CircleSelector'>
+            <Divider horizontal>
+              <Header as='h4'>
+                <Icon name='left quote' />
+                Mission Statement
+              </Header>
+            </Divider>
+            <Message floating content='As a software engineer, I endeavor to work on projects that require a synergy of logic, analytical methodologies, and creativity. I have experience in entrepreneurial development, product development / design, as well as tabletop game design. I have honed my leadership skills through the military and research acumen while earning a bachelors of science degree in applied psychology.
             
-            {props.ballNo.map((element, idx) =>
-                <button
-                    
-                    className={props.ballIdx === idx ? 'selected' : ''}
-                    onClick={() => props.handleSummon(idx)}
-                >
-                    {props.ballIdx === idx ? <img 
-                        height='60'src='https://media0.giphy.com/media/3oEjI6SIIHBdRxXI40/200.gif' alt='Hadouken' /> : `Push for ` } {idx + 1}
-                </button>
-            )}
+            I endeavor to become a seasoned software engineer by continuing to learn new concepts as well as iterating upon what I already know. ' /> 
         </div>
     )
 }
