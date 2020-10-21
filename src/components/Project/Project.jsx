29594005ee9a5184d2d1d1e7c15da91cc3dc6b6b
A.I. Kasem <ak89mkii@gmail.com>
2020-10-20 20:21:53 -0400

    Project card

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Header, Icon, Image, Card, Button, Divider } from 'semantic-ui-react'
+import { Header, Icon, Image, Card, Button, Divider, Grid } from 'semantic-ui-react'
+import './Project.css'
 
 const Project = () => {
     return (
@@ -11,11 +12,15 @@ const Project = () => {
               </Header>
             </Divider>
 
+            <div className='grid'>
+            <Grid centered>
+            <Grid.Row centered columns={4}>
+            <Grid.Column>
+
             <Card>
                 <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
                 <Card.Content>
                 <Card.Header>War Card Game</Card.Header>
-                
                 <Card.Description>
                     Technologies Used:
                     <ul>
@@ -26,16 +31,97 @@ const Project = () => {
                     </ul>
                 </Card.Description>
                     <h3>
-                    <a href='https://github.com/ak89mkii/war-project-01'>
-                    <Button size='large'><Icon size='large' name='github square'/>Github Repo</Button> 
-                    </a>
-                    
+                        <a href='https://github.com/ak89mkii/war-project-01'>
+                        <Button size='large'>
+                            <Icon size='large' name='github square'/>Github Repo
+                        </Button> 
+                        </a>
                     </h3>
-                    
                 </Card.Content>
             </Card>
 
+            </Grid.Column>
+            <Grid.Column>
+
+            <Card>
+                <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+                <Card.Content>
+                <Card.Header>War Card Game</Card.Header>
+                <Card.Description>
+                    Technologies Used:
+                    <ul>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                </Card.Description>
+                    <h3>
+                        <a href='https://github.com/ak89mkii/war-project-01'>
+                        <Button size='large'>
+                            <Icon size='large' name='github square'/>Github Repo
+                        </Button> 
+                        </a>
+                    </h3>
+                </Card.Content>
+            </Card>
+
+            </Grid.Column>
+            <Grid.Column>
+
+            <Card>
+                <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+                <Card.Content>
+                <Card.Header>War Card Game</Card.Header>
+                <Card.Description>
+                    Technologies Used:
+                    <ul>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                </Card.Description>
+                    <h3>
+                        <a href='https://github.com/ak89mkii/war-project-01'>
+                        <Button size='large'>
+                            <Icon size='large' name='github square'/>Github Repo
+                        </Button> 
+                        </a>
+                    </h3>
+                </Card.Content>
+            </Card>
             
+            </Grid.Column>
+            <Grid.Column>
+
+            <Card>
+                <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+                <Card.Content>
+                <Card.Header>War Card Game</Card.Header>
+                <Card.Description>
+                    Technologies Used:
+                    <ul>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                </Card.Description>
+                    <h3>
+                        <a href='https://github.com/ak89mkii/war-project-01'>
+                        <Button size='large'>
+                            <Icon size='large' name='github square'/>Github Repo
+                        </Button> 
+                        </a>
+                    </h3>
+                </Card.Content>
+            </Card>
+
+            </Grid.Column>
+            </Grid.Row>
+            </Grid>
+            </div>
         </div>
     )
 }
