f640a4cdc6b35ac6fb3bbaf9c33085f9a93ae321
A.I. Kasem <ak89mkii@gmail.com>
2020-10-22 16:41:26 -0400

    Completed MVP

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header, Icon, Image, Menu, Message, Divider } from 'semantic-ui-react'
+import { Header, Icon, Segment, List, Card, Divider, Button } from 'semantic-ui-react'
 
 
 const Contact = () => {
@@ -12,6 +12,38 @@ const Contact = () => {
               </Header>
             </Divider>
            
+            <Card centered>
+            <Segment inverted>
+                <List divided inverted relaxed>
+                <List.Item>
+                    <List.Content>
+                    <List.Header>Email Address:</List.Header>
+                    aikasem@gmail.com
+                    </List.Content>
+                </List.Item>
+                <List.Item>
+                    <List.Content>
+                    <List.Header></List.Header>
+                    <a href='https://www.linkedin.com/in/alexander-kasem-950479105/'>
+                    <Button color='linkedin'>
+                        <Icon name='linkedin' /> LinkedIn
+                    </Button>
+                    </a>
+                    </List.Content>
+                </List.Item>
+                <List.Item>
+                    <List.Content>
+                    <List.Header></List.Header>
+                    <a href='https://www.linkedin.com/in/alexander-kasem-950479105/'>
+                    <Button color='github'>
+                        <Icon name='github' /> GitHub
+                    </Button>
+                    </a>
+                    </List.Content>
+                </List.Item>
+                </List>
+            </Segment>
+            </Card>
         </div>
     )
 }
