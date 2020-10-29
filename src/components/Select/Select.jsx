5307ce4ef08c6b2f6bd57a07adc353b2de244309
A.I. Kasem <ak89mkii@gmail.com>
2020-10-28 22:30:21 -0400

    Toggle view

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Image, Icon, Button, Grid } from 'semantic-ui-react'
+import { Image, Icon, Button, Grid, Header,Segment, TransitionablePortal,} from 'semantic-ui-react'
 import Fight from '../../Img/fight.png'
 import Music from '../../components/sounds/music.wav'
 import './Select.css'
@@ -7,8 +7,12 @@ import ReactHowler from 'react-howler'
 
 class Select extends Component {
   state = {
-    onMusic: false
+    onMusic: false,
+    open: false
   }
+
+  handleClick = () => this.setState((prevState) => ({ open: !prevState.open }))
+  handleClose = () => this.setState({ open: false })
 
   handleToggle02 = () => {
     this.setState({
@@ -17,6 +21,7 @@ class Select extends Component {
   }
 
     render() {
+      const { open } = this.state
       return (
       <div>
         {this.state.onMusic && (<ReactHowler
@@ -28,6 +33,19 @@ class Select extends Component {
           centered
           src={Fight}
         />
+        <br></br>
+        <br></br>
+        <Grid centered>
+        <Button
+          size='huge'
+          content={open ? 'Close Skill List' : 'Open Skill List'}
+          negative={open}
+          positive={!open}
+          onClick={this.handleClick}
+        />
+        </Grid> 
+        <br></br>
+        <br></br>
         <Grid centered>
           <Button 
             color='blue' 
@@ -39,6 +57,18 @@ class Select extends Component {
             <Icon centered name='volume off'/>
           </Button>
         </Grid>
+      <div>
+        <TransitionablePortal onClose={this.handleClose} open={open}>
+          <Segment
+            style={{ left: '60%', position: 'fixed', top: '50%', zIndex: 1000 }}
+          >
+            <Header>Technology List:</Header>
+            <p>Row 01: HTML5, CSS3, JavaScript, Python 3</p>
+            <p>Row 02: React.ja, Node.js, Express.js, Django, MongoDB, PostreSQL</p>
+            <p>Row 03: Materialize, Bootstrap, Semantic UI React, GitHub, Heroku, Coffee (the drink)</p>
+          </Segment>
+        </TransitionablePortal>
+      </div>
         <br></br>
         <br></br>
       </div>
