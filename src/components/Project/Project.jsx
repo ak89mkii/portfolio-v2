7d4a3b08c6eb89f7de62ff178ded19dea592254c
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2021-02-28 14:39:48 -0500

    Fixed PAJ and RepBucket cards.

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -17,6 +17,7 @@ const Project = () => {
               </Header>
             </Divider>
 
+            {/* Row 01*/}
             <Item.Group>
             <Grid columns={2} divided stackable>
                 <Grid.Row verticalAlign='middle'>
@@ -61,22 +62,25 @@ const Project = () => {
                                 <Item.Header>DiceDex: Tabletop Tracker(RESTful, CRD)
                                 </Item.Header>
                                 <Item.Description>
-                                    <p>A War playing card variant with a retro game theme.</p>
+                                    <p>DiceDex is an application that enables groups of users to create shared tabletop libraries, wishlists, as well as leave comments on a message board.</p>
                                     <h4>Technologies Used:</h4>
                                         <p>- HTML5</p>
                                         <p>- CSS3</p>
-                                        <p>- JavaScript</p>
+                                        <p>- Express.js</p>
+                                        <p>- Node.js</p>
+                                        <p>- MongoDB</p>
+                                        <p>- Mongoose JS</p>
                                         <p>- Bootstrap</p>
                                 </Item.Description>
                                     <h3>
-                                        <a href='http://war-were-declared-4.surge.sh/' target="_blank">
+                                        <a href='https://tabletop-tracker-project-117.herokuapp.com/users' target="_blank">
                                         <Button size='large' color='yellow'>
                                             <Icon size='large' name='power off'/>View Project
                                         </Button> 
                                         </a>
                                         <br></br>
                                         <br></br>
-                                        <a href='https://github.com/ak89mkii/war-project-01' target="_blank">
+                                        <a href='https://github.com/ak89mkii/tabletop-tracker-project-01' target="_blank">
                                         <Button size='large'>
                                             <Icon size='large' name='github square'/>Github Repo
                                         </Button> 
@@ -85,93 +89,91 @@ const Project = () => {
                             </Card.Content>
                         </Card>
                     </Grid.Column>
+                </Grid.Row>
+            </Grid>
+            </Item.Group>
 
+            {/* Row 02 */}
+            <Item.Group>
+            <Grid columns={2} divided stackable>
+                <Grid.Row verticalAlign='middle'>
+                    
+                    <Grid.Column>   
+                        <Card fluid centered size='large' stackable>
+                            <Card.Content>
+                                <Item.Image src={paj} size='large' wrapped ui={false} />
+                                <Divider/>
+                                <Item.Header>PAJ: Project Manager (Team Project, MERN)</Item.Header>
+                                <Item.Description>
+                                    <p>PAJ, the Project Assistance Journal, is a platform that enables users to add and monitor their project progressions as well facilitating collaborative efforts.</p>
+                                    <h4>Technologies Used:</h4>
+                                        <p>- HTML5</p>
+                                        <p>- CSS3</p>
+                                        <p>- React.js</p>
+                                        <p>- Express.js</p>
+                                        <p>- Node.js</p>
+                                        <p>- MongoDB</p>
+                                        <p>- Semantic UI React</p>
+                                </Item.Description>
+                                    <h3>
+                                        <a href='https://paj-03-project-hi-nu.herokuapp.com/' target="_blank">
+                                        <Button size='large' color='yellow'>
+                                            <Icon size='large' name='power off'/>View Project
+                                        </Button> 
+                                        </a>
+                                        <br></br>
+                                        <br></br>
+                                        <a href='https://github.com/ak89mkii/PAJ' target="_blank">
+                                        <Button size='large'>
+                                            <Icon size='large' name='github square'/>Github Repo
+                                        </Button> 
+                                        </a>
+                                    </h3>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                    
+                    <Grid.Column> 
+                    <Card fluid centered size='large' stackable>
+                            <Card.Content>
+                                <Item.Image src={repbucket} size='large' wrapped ui={false} />
+                                <Divider/>
+                                <Item.Header>DiceDex: Tabletop Tracker(RESTful, CRD)
+                                </Item.Header>
+                                <Item.Description>
+                                    <p>RepBucket is a platform that "gamifies" the tracking and highlighting of achievements, activities, skills, hobbies, and goals. From the mundane to monumental, goals are your "Quests", which can be found in your "Quest Journal". Your skills and hobbies are represented as skills on your "Talent Tree". Level them up as you see fit.</p>
+                                    <h4>Technologies Used:</h4>
+                                        <p>- HTML5</p>
+                                        <p>- CSS3</p>
+                                        <p>- Python 3</p>
+                                        <p>- Django</p>
+                                        <p>- PostgreSQL</p>
+                                        <p>- Bootstrap</p>
+                                </Item.Description>
+                                    <h3>
+                                        <a href='https://repbucket-04.herokuapp.com/' target="_blank">
+                                        <Button size='large' color='yellow'>
+                                            <Icon size='large' name='power off'/>View Project
+                                        </Button> 
+                                        </a>
+                                        <br></br>
+                                        <br></br>
+                                        <a href='https://github.com/ak89mkii/Repbucket-project-04' target="_blank">
+                                        <Button size='large'>
+                                            <Icon size='large' name='github square'/>Github Repo
+                                        </Button> 
+                                        </a>
+                                    </h3>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
                 </Grid.Row>
             </Grid>
             </Item.Group>
                
-            {/* <Card centered>
-                <Image src={dicedex} wrapped ui={false} />
-                <Card.Content>
-                <Card.Header>DiceDex: Tabletop Tracker</Card.Header>
-                (RESTful, CRD) 
-                <p>DiceDex is an application that enables groups of users to create shared tabletop libraries, wishlists, as well as leave comments on a message board.</p>
-                <Card.Description>
-                    Technologies Used:
-                    <ul>
-                        <li>HTML5</li>
-                        <li>CSS3</li>
-                        <li>Express.js</li>
-                        <li>Node.js</li>
-                        <li>MongoDB</li>
-                        <li>Mongoose JS</li>
-                        <li>Bootstrap</li>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                    </ul>
-                </Card.Description>
-                    <h3>
-                        <a href='https://tabletop-tracker-project-117.herokuapp.com/users' target="_blank">
-                        <Button size='large' color='yellow'>
-                            <Icon size='large' name='power off'/>View Project
-                        </Button> 
-                        </a>
-                        <br></br>
-                        <br></br>
-                        <a href='https://github.com/ak89mkii/tabletop-tracker-project-01' target="_blank">
-                        <Button size='large'>
-                            <Icon size='large' name='github square'/>Github Repo
-                        </Button> 
-                        </a>
-                    </h3>
-                </Card.Content>
-            </Card>
+            {/* 
 
-            </div>
-            <div className="column">
-
-            <Card centered>
-                <Image src={paj} wrapped ui={false} />
-                <Card.Content>
-                <Card.Header>PAJ: Project Manager</Card.Header>
-                (Team Project, MERN) 
-                <p>PAJ, the Project Assistance Journal, is a platform that enables users to add and monitor their project progressions as well facilitating collaborative efforts.</p>
-                <Card.Description>
-                    Technologies Used:
-                    <ul>
-                        <li>HTML5</li>
-                        <li>CSS3</li>
-                        <li>React.js</li>
-                        <li>MongoDB</li>
-                        <li>Semantic UI React</li>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                    </ul>
-                </Card.Description>
-                    <h3>
-                        <a href='https://paj-03-project-hi-nu.herokuapp.com/' target="_blank">
-                        <Button size='large' color='yellow'>
-                            <Icon size='large' name='power off'/>View Project
-                        </Button> 
-                        </a>
-                        <br></br>
-                        <br></br>
-                        <a href='https://github.com/ak89mkii/PAJ' target="_blank">
-                        <Button size='large'>
-                            <Icon size='large' name='github square'/>Github Repo
-                        </Button> 
-                        </a>
-                    </h3>
-                </Card.Content>
-            </Card>
+            
             
             </div>
             <div className="column">
