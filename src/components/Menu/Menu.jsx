a270559c4768fd0c8103171fa566b14c28aadca5
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2021-09-08 13:27:41 -0400

    Added munu component.

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,101 @@
+import React, { Component, createRef } from 'react'
+import '../../App.css'
+import 'semantic-ui-css/semantic.min.css'
+import { Button, Grid, Sticky, Menu, Segment, Divider } from 'semantic-ui-react'
+import Statement from '../../components/Statement/Statement'
+import Project from '../../components/Project/Project'
+import Interest from '../../components/Interest/Interest'
+import Contact from '../../components/Contact/Contact'
+import Intro from '../../components/Intro/Intro'
+import Select from '../../components/Select/Select'
+import Skill from '../../components/Skill/Skill'
+import Audio from '../../sounds/select.mp3'
+import ReactHowler from 'react-howler'
+
+class Menu extends Component {
+    state = {
+        activeItem: 'Mission',
+        on: false,
+        sound: false,
+        onMusic: false
+    }
+
+    contextRef = createRef()
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    handleToggle = () => {
+        this.setState({
+        on: !this.state.on,
+        sound: !this.state.sound
+        })
+    }
+
+    handleToggle02 = () => {
+        this.setState({
+        sound: !this.state.sound
+        })
+    }
+
+    render() {
+      const { activeItem } = this.state
+        return (
+            <div>
+                <br></br>
+                <div ref={this.contextRef}>
+                <Sticky context={this.contextRef}>
+                <Segment inverted>
+                <Menu
+                inverted
+                pointing
+                secondary
+                size='large'
+                attached='top'
+                // stackable
+                >
+                    <a href='#mission'>
+                    <Menu.Item
+                    name='Mission'
+                    // active={activeItem === 'Mission'}
+                    onClick={this.handleItemClick}
+                    />
+                    </a>
+                    <a href='#projects'>
+                    <Menu.Item
+                    name='Projects'
+                    // active={activeItem === 'Projects'}
+                    onClick={this.handleItemClick}
+                    />
+                    </a>
+                    <a href='#skills'>
+                    <Menu.Item
+                    name='Skills'
+                    // active={activeItem === 'Skills'}
+                    onClick={this.handleItemClick}
+                    />
+                    </a>
+                    <a href='#interests'>
+                    <Menu.Item
+                    name='Interests'
+                    // active={activeItem === 'Interests'}
+                    onClick={this.handleItemClick}
+                    />
+                    </a>
+                    <a href='#contact'>
+                    <Menu.Item
+                    name='Contact'
+                    // active={activeItem === 'Contact'}
+                    onClick={this.handleItemClick}
+                    />
+                    </a>
+                    </Menu>
+                </Segment>
+                </Sticky>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Menu;
+
