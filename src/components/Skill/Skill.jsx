483095a4b56dc54388c443cc4d620aef4c648b09
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2021-09-19 13:58:30 -0400

    Working on front-end project cards, skills section.

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -14,13 +14,13 @@ const items = [
       childKey: 1,
       image: 'https://pbs.twimg.com/profile_images/749981640609398784/-amtsDMY_400x400.jpg',
       header: <h2 className='left'>Familiar Technologies:</h2>,
-      description: <div><p>The technologies and languages that I am familiar with.</p><p>Node.js | Express.js | Django | Flask | MongoDB | PostgreSQL | Boostrap | C# | Unity Engine</p><img height='70px' src='https://images.tute.io/tute/topic/express-js.png'/><img height='70px' src='https://miro.medium.com/max/1200/1*1OBwwxzJksMv0YDD-XmyBw.png'/><img margin='10px' height='70px' src='https://www.pngfind.com/pngs/m/104-1044449_python-logo-clipart-drawing-flask-python-hd-png.png'/><img height='70px' src='https://infinapps.com/wp-content/uploads/2018/10/mongodb-logo.png'/><img height='70px' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png'/><img height='70px' src='https://freepikpsd.com/media/2019/10/bootstrap-logo-png-1-Transparent-Images-Free.png'/><img margin='10px' height='70px' src='https://seeklogo.com/images/C/c-sharp-c-logo-02F17714BA-seeklogo.com.png'/></div>,
+      description: <div><p>The technologies and languages that I am familiar with.</p><p>Node.js | Express.js | Django | Flask | MongoDB | PostgreSQL | Boostrap | C# (withUnity Engine)</p><img height='70px' src='https://images.tute.io/tute/topic/express-js.png'/><img height='70px' src='https://miro.medium.com/max/1200/1*1OBwwxzJksMv0YDD-XmyBw.png'/><img margin='10px' height='70px' src='https://www.pngfind.com/pngs/m/104-1044449_python-logo-clipart-drawing-flask-python-hd-png.png'/><img height='70px' src='https://infinapps.com/wp-content/uploads/2018/10/mongodb-logo.png'/><img height='70px' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png'/><img height='70px' src='https://freepikpsd.com/media/2019/10/bootstrap-logo-png-1-Transparent-Images-Free.png'/><img margin='10px' height='70px' src='https://seeklogo.com/images/C/c-sharp-c-logo-02F17714BA-seeklogo.com.png'/></div>,
   },
   {
       childKey: 2,
       image: 'https://www.kindpng.com/picc/m/35-350705_skill-icon-png-logo-for-resume-skill-transparent.png',
-      header: <h2 className='left'>Core Competencies:</h2>,
-      description: <div><p>Critical Thinking:</p><p>Research:</p><p>Teamwork:</p><p>Continuous Learner:</p></div>,
+      header: <h2 className='left'>Other Skills:</h2>,
+      description: <div><p>Experience from the military, college education, designing board games, inventing, and interest in sci-tech.</p><p>Critical Thinker | Researcher | Teamwork | Continuous Learner</p></div>,
   },
 ]
 
