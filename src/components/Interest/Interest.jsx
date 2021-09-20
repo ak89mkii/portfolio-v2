f85a49917c11a9bcea719059e8119e626c5ce873
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2021-09-20 16:55:53 -0400

    Working on front-end project cards.

--- src/components/Interest/Interest.jsx
+++ src/components/Interest/Interest.jsx
@@ -32,7 +32,7 @@ const Interest = (props) => {
 
                         <Image src='https://i.pinimg.com/originals/48/1f/64/481f64d751427db0e7f10c9bc2cd2701.gif' height='100px' verticalAlign='middle' />{' '}
                         <h3>Boarding</h3>
-                        <span>I enjoy snowboarding and skateboading, though I am a bit better at the former than the latter.</span>
+                        <span>I enjoy snowboarding and skateboading, though I am a bit better at the former than the latter. I intend on getting a One Wheel at some point.</span>
                         <Divider />
 
                         
@@ -46,7 +46,7 @@ const Interest = (props) => {
 
                         <Image src='https://cdn.dribbble.com/users/2245526/screenshots/4476814/chemex-animation_loop.gif' height='100px' verticalAlign='middle' />{' '}
                         <h3>Coffee</h3>
-                        <span>One of my favorite beverages, I tend to favor drinking coffee without anything added to it. Using a Chemex is one of my preferred methods of brewing good coffee.</span>
+                        <span>Coffee, black. One of my favorite beverages. Using a Chemex is one of my preferred methods of brewing good coffee.</span>
                         <Divider />
 
                         <Image src='https://cdn.dribbble.com/users/623144/screenshots/2077826/process-concept_dribbble.gif' height='100px' verticalAlign='middle' />{' '}
