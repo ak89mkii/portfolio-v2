c8da273fd3bb533b8fced0a41894742ea24b0217
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-02-18 21:34:57 -0500

    Updated LinkedIn link.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Header, Icon, Segment, List, Card, Divider, Button, TransitionablePortal } from 'semantic-ui-react'
-import Resume from '../../Img/resume02.PDF'
+import Resume from '../../Img/resume03.PDF'
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 class Contact extends Component {
